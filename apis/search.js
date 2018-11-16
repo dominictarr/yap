d8fccb46889e0607fbeb90a89f8c4f836d81d4a3
Dominic Tarr <dominic.tarr@gmail.com>
2018-11-17 06:43:50 +1300

    search accepts msg and feed links

--- apis/search.js
+++ apis/search.js
@@ -1,9 +1,15 @@
 var pull = require('pull-stream')
-var render = require('./message').render
+var ref = require('ssb-ref')
 
 module.exports = function (opts) {
   var self = this
+  var api = this.api
   opts.limit = opts.limit || 10
+  if(ref.isMsgLink(opts.query))
+    return api('message', {id: opts.query})
+  if(ref.isFeed(opts.query))
+    return api('public', {id: opts.query})
+
   return pull(
     self.sbot.search.query(opts),
     pull.map(function (data) {
@@ -11,4 +17,6 @@ module.exports = function (opts) {
     })
   )
 }
+
+
 
