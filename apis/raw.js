bf1863f1e919bc8cfce2eaf6b437067917f2a595
Dominic Tarr <dominic.tarr@gmail.com>
2018-11-17 06:39:36 +1300

    raw view should use u.createRenderer

--- apis/raw.js
+++ apis/raw.js
@@ -1,10 +1,10 @@
+var u = require('../util')
+module.exports = u.createRenderer(function (data) {
+  return ['pre', JSON.stringify(data, null, 2)]
+})
 
-module.exports = function (opts) {
-  var sbot = this.sbot
-  return function (cb) {
-    sbot.get(opts, function (err, value) {
-      if(err) cb(err)
-      else cb(null, ['pre', JSON.stringify(value, null, 2)])
-    })
-  }
-}
+
+
+
+
+
