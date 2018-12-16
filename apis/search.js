c343d27955954f95f2c6ce3302289420b7e1e075
Dominic Tarr <dominic.tarr@gmail.com>
2018-12-17 00:35:57 +1300

    can type in @name and get to feed quickly

--- apis/search.js
+++ apis/search.js
@@ -10,6 +10,24 @@ module.exports = function (opts) {
   if(ref.isFeed(opts.query))
     return api('public', {id: opts.query})
 
+  if(/^@\w+$/.test(opts.query)) //is name
+    return function (cb) {
+      self.sbot.names.getSignifies(opts.query.substring(1), function (err, ids) {
+        if(err) return cb(err)
+        if(ids.length == 0)
+          cb(null, ['h1', 'no feed named:'+opts.query])
+        else if(ids.length == 1)
+          cb(null, api('public', {author: ids[0].id}))
+        else
+          cb(null, ['div',
+            ['div', opts.query + ' may also refer to:'].concat(ids.slice(1).map(function (e) {
+              return api('avatar', {id: e.id, name: false})
+            })),
+            api('public', {author: ids[0].id})
+          ])
+      })
+    }
+
   return pull(
     self.sbot.search.query(opts),
     pull.map(function (data) {
@@ -17,6 +35,10 @@ module.exports = function (opts) {
     })
   )
 }
+
+
+
+
 
 
 
