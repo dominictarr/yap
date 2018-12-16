37158d59e85565e399e8af8ef0c05f27261fdb81
Dominic Tarr <dominic.tarr@gmail.com>
2018-12-16 22:12:15 +1300

    forwards and back links at the top and bottom of each page

--- apis/public.js
+++ apis/public.js
@@ -10,46 +10,79 @@ var h = u.h
 toUrl = u.toUrl
 
 var render = require('./message').render
+var niceAgo = require('nice-ago')
 
 module.exports = function (opts) {
   var self = this
   var sbot = this.sbot, api = this.api
   opts.reverse = opts.reverse !== false
   var min = !isNaN(+opts.lt) ? +opts.lt : Date.now()
+  var max = !isNaN(+opts.gt) ? +opts.gt : 0
 
   var type = (opts.private ? 'private' : 'public')
   var Type = (opts.private ? 'Private' : 'Public')
 
   return h('div.' + Type,
     h('title', Type),
-    pull(
-      sbot.query.read({
-        query: [{$filter: {
-          value: {
-            content: {
-              type: 'post',
-              channel: opts.channel,
+    function (cb) {
+      pull(
+        sbot.query.read({
+          query: [{$filter: {
+            value: {
+              content: {
+                type: 'post',
+                channel: opts.channel,
+              },
+              author: opts.author,
+              private: opts.private ? true : {$is: 'undefined'},
+              timestamp: max ? {$gt: max} : min ? {$lt: min} : undefined
             },
-            author: opts.author,
-            private: opts.private ? true : {$is: 'undefined'},
-            timestamp: min ? {$lt: min} : undefined
-          },
-        }}],
-        limit: 20, reverse: true
-      }),
-      pull.map(function (data) {
-        min = Math.min(data.value.timestamp, min)
-        return api('message', data)
-      }),
-      Append(function last () {
-        return [
-          'a', {
-            href: toUrl(type, Object.assign({}, opts, {lt: min}))
-          },
-          'Load More'
-        ]
-      })
-    )
+          }}/*, {
+            $sort: [['value', 'timestamp']]
+          }*/],
+          limit: 20, reverse: max ? false : true
+        }),
+        pull.collect(function (err, ary) {
+          if(err) return cb(err)
+          ary = ary.sort(function (a, b) {
+            return b.value.timestamp - a.value.timestamp
+          }).map(function (data) {
+            min = Math.min(data.value.timestamp, min)
+            max = Math.max(data.value.timestamp, max)
+            return api('message', data)
+
+          })
+          var nav_opts = {}
+          if(opts.author)
+            nav_opts.author = opts.author
+
+          var nav = ['span',
+            '<< ',
+            opts.author ? api(['avatar'], {id: opts.author, name: true, image: false}) : '',
+            ' ',
+            ['a', {
+                href: toUrl(type, Object.assign({}, nav_opts, {
+                  gt: max
+                })),
+                title: new Date(max).toString()
+              },
+            niceAgo(Date.now(), max)
+            ], ' + ',
+            ['a', {
+                href: toUrl(type, Object.assign({}, nav_opts, {
+                  lt: min
+                })),
+                title: new Date(max).toString()
+              },
+              niceAgo(Date.now(), min)],
+            ' >>'
+          ]
+          ary.unshift(nav)
+          ary.push(nav)
+          cb(null, ary)
+        })
+      )
+    }
   )
 }
 
