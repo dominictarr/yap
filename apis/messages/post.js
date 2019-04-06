5540d1d97a2162246474b77a06331641ef828f45
Dominic Tarr <dominic.tarr@gmail.com>
2019-04-06 15:28:10 +1300

    make it look a bit more like patchbay

--- apis/messages/post.js
+++ apis/messages/post.js
@@ -9,28 +9,38 @@ toUrl = u.toUrl
 module.exports = u.createRenderer(function render (data, apply) {
   var since = apply.since
   var time = data.value.timestamp || data.timestamp
-  return h('div.Message',
+  return ['div.Message',
     u.cacheTag(toUrl('message', {id: data.key}), data.key, since),
-    h('div.MessageMeta',
-      apply('avatar', {id: data.value.author, name: true, image: true}),
+    ['div.MessageSide',
+      apply('avatar', {id: data.value.author, name: false, image: true}),
       h('a', {
         href: toUrl('message', {id: data.key}),
         title: new Date(time)+'\n'+data.key
       },
         ''+niceAgo(Date.now(), time)
-      ),
-      h('label.type', data.value.content.type),
-      h('label.msgId', data.key),
+      )
+    ],
+    ['div.MessageMain',
+      ['div.MessageMeta',
+        apply('avatar', {id: data.value.author, name: true, image: false}),
+        /*
+        h('label.type', data.value.content.type),
+        */
+        h('label.msgId', data.key),
 
-      data.value.content.channel
-        ? h('a', {href: toUrl('public', {channel: data.value.content.channel})}, '#'+data.value.content.channel)
-        : '',
+        data.value.content.channel
+          ? h('a', {href: toUrl('public', {channel: data.value.content.channel})}, '#'+data.value.content.channel)
+          : '',
 
-      h('a', {href: toUrl('thread', {id: data.value.content.root || data.key})}, 'Thread')
+        h('a', {href: toUrl('thread', {id: data.value.content.root || data.key})}, 'Thread')
 
-    ),
-    h('div.MessageContent', u.markdown(data.value.content))
-  )
+      ],
+      ['div.MessageContent', u.markdown(data.value.content)]
+    ]
+  ]
 })
+
+
+
 
 
