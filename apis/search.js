7c2d1c9fac2d14235c39355c2a4a829c6f34e73a
Dominic Tarr <dominic.tarr@gmail.com>
2019-04-16 20:35:42 +1200

    search for a channel name just dumps you into that channel

--- apis/search.js
+++ apis/search.js
@@ -1,26 +1,37 @@
 var pull = require('pull-stream')
 var ref = require('ssb-ref')
 
+function isChannel (c) {
+  return /^#\w+/.test(c)
+}
+
+function isName (c) {
+  return /^@\w+/.test(c)
+}
+
 module.exports = function (sbot) {
   return function (opts, apply, req) {
     var tr = require('../translations')(req.cookies.lang)
+    var query = (opts.query || '').trim()
     opts.limit = opts.limit || 10
-    if(ref.isMsgLink(opts.query))
-      return apply('message', {id: opts.query})
-    if(ref.isFeed(opts.query))
-      return apply('public', {id: opts.query})
+    if(ref.isMsgLink(query))
+      return apply('message', {id: query})
+    if(ref.isFeed(query))
+      return apply('public', Object.assign(opts, {id: query}))
+    if(isChannel(query))
+      return apply('public', Object.assign(opts, {channel: query.substring(1)}))
 
-    if(/^@\w+$/.test(opts.query)) //is name
+    if(isName(query)) //is name
       return function (cb) {
-        sbot.names.getSignifies(opts.query.substring(1), function (err, ids) {
+        sbot.names.getSignifies(query.substring(1), function (err, ids) {
           if(err) return cb(err)
           if(ids.length == 0)
-            cb(null, ['h1', tr('NoFeedNamed'), opts.query])
+            cb(null, ['h1', tr('NoFeedNamed'), query])
           else if(ids.length == 1)
             cb(null, apply('public', {author: ids[0].id}))
           else
             cb(null, ['div',
-              ['div', opts.query, ' ', tr('MayAlsoBe')].concat(ids.slice(1).map(function (e) {
+              ['div', query, ' ', tr('MayAlsoBe')].concat(ids.slice(1).map(function (e) {
                 return apply('avatar', {id: e.id, name: false})
               })),
               apply('public', {author: ids[0].id})
